6ac938b134c3dbfbf8d124eece609e99770b3eee
IvanPopovic1991 <iki.popovic991@gmail.com>
2024-05-27 15:55:01 +0200

    - Updated get all users test (get users request).

--- src/main/java/modal/UserResponse.java
+++ src/main/java/modal/UserResponse.java
@@ -1,6 +1,7 @@
 package modal;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 import java.util.Date;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
